9e5ccfe6744875fda72d2c39096ed791d4701ebf
Carlos Mejia <mejia_98@hotmail.com>
2020-03-24 15:17:46 -0500

    AddDeleteCategories
    Add view of delete categories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,15 @@ import { LoginComponent } from './components/login/login.component';
 import { IndexComponent } from './components/dashboard/index/index.component';
 import { CategoriesComponent } from './components/dashboard/categories/categories.component';
 import { AddCategoriesComponent } from './components/add-categories/add-categories.component';
+import { DeleteCategorieComponent } from './components/delete-categorie/delete-categorie.component';
 
-const routes: Routes = [{ path: "", component: LoginComponent }, { path: "index", component: IndexComponent }, { path: "categorias", component: CategoriesComponent }, { path: "agregar", component: AddCategoriesComponent },];
+const routes: Routes = [
+  { path: "", component: LoginComponent }, 
+  { path: "index", component: IndexComponent }, 
+  { path: "categorias", component: CategoriesComponent }, 
+  { path: "agregar", component: AddCategoriesComponent },
+  { path: "eliminar", component: DeleteCategorieComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
