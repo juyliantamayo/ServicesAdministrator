71e88a6d7980b6d53256dfb4a9717c1dc36105bf
Carlos Mejia <mejia_98@hotmail.com>
2020-04-01 14:30:05 -0500

    ServicesXmonth-Chart

--- src/app/components/dashboard/index/index.component.ts
+++ src/app/components/dashboard/index/index.component.ts
@@ -5,6 +5,9 @@ import { CategoriesService } from 'src/app/services/categories/categories.servic
 import { ServicesService } from 'src/app/services/services/services.service';
 import { Categori } from 'src/app/models/categories';
 import { Graficas } from 'src/app/models/graficas';
+import { async } from 'rxjs/internal/scheduler/async';
+import * as moment from 'moment';
+
 declare var $: any;
 @Component({
   selector: 'app-index',
@@ -12,7 +15,20 @@ declare var $: any;
   styleUrls: ['./index.component.css']
 })
 export class IndexComponent implements OnInit {
+
+  // options
+  showXAxisSer = true;
+  showYAxisSer = true;
+  gradientSer = false;
+  showLegendSer = true;
+  showXAxisLabelSer = true;
+  xAxisLabelSer = 'Meses';
+  showYAxisLabelSer = true;
+  yAxisLabelSer = 'Servicios Creados';
+  labelStringDataFirschar2: any[];
+
   gradientpie: boolean = false;
+
 
   showLabels: boolean = true;
   isDoughnut: boolean = false;
@@ -28,7 +44,7 @@ export class IndexComponent implements OnInit {
   showYAxisLabel = true;
   yAxisLabel = 'Servicios \n creados';
   colorScheme = {
-    domain: ['#5AA454', '#A10A28', '#C7B42C', '#AAAAAA']
+    domain: ['#994df7', '#ec4391', '#357e5f', '#e8f74d', '#AAAAAA']
   };
   constructor(private auth: AuthService, private categoryservice: CategoriesService, private serviceService: ServicesService) {
 
@@ -39,6 +55,48 @@ export class IndexComponent implements OnInit {
   }
 
   async ngOnInit(): Promise<void> {
+
+    var meses = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+
+    //Funcion para agregar datos a la grafica de servicios por mes.
+    await this.serviceService.obtenerServices().subscribe((data) => {
+      let datachartpibot: Array<Graficas> = new Array<Graficas>();
+      let contador: number = 0;
+
+      data.forEach((servicio) => {
+        var fecha: string = moment(servicio.payload.doc.data()['createdAt'].toDate()).format("MMMM");
+        console.log("Mes: " + fecha);
+        let graficapibote: Graficas = new Graficas();
+        
+        for (let index = 0; index < meses.length; index++) {
+
+          var element = meses[index].toString();
+
+          if (element == fecha) {
+            graficapibote.name = meses[index].toString();
+            graficapibote.value = contador++;
+            console.log("Se agrego en el mes = " + meses[index].toString() + " hay " + contador)
+          } else {
+            console.log("no entro");
+          }
+        }
+        datachartpibot.push(graficapibote);
+      })
+      this.labelStringDataFirschar2 = JSON.parse(JSON.stringify(datachartpibot));
+    })
 
     await this.categoryservice.obtenerCategorias().subscribe(async (data) => {
 
